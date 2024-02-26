6e4dc4e39de7471860e6f89e5d24ee973f6f1866
vbudithi <vivek.budithi@gmail.com>
2024-02-27 02:19:54 +0930

    Rapid API Created

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,19 @@
 //This component handles two primary parts Sidebar and video feed
 
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/FetchRouteAPI';
+import React, { useEffect, useState } from 'react';
 
-function Feed() {
+
+const Feed = () => {
+
+    const [selectedCategory, setSelectedCategory] = useState("New");
+
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    }, [selectedCategory]);
+
     return (
         <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
             <Box
@@ -11,26 +21,51 @@ function Feed() {
                     height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d',
                     px: { sx: 0, md: 2 }
                 }}>
-                <Sidebar />
-
+                <Sidebar
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
             </Box>
-            <Typography
-                className="copyright"
-                variant="body2"
-                align="center"
+            <Box
+                p={3}
                 sx={{
-                    position: 'fixed',
-                    left: 0,
-                    bottom: 0,
-                    width: '100%',
-                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    color: '#fff',
-                    py: { sx: 1, md: 0 },
-                    zIndex: 1000
-                }}
-            >
-                Copyright@2023 CodeVivek. All rights reserved
-            </Typography>
+                    overflowY: 'auto',
+                    height: '90vh',
+                    flex: 2,
+                    textAlign: 'center'
+                }}>
+                <Typography
+                    variant="h4"
+                    fontWeight="bold"
+                    mb={2}
+                    sx={{ color: 'white' }}
+                >
+                    {selectedCategory} <span style={{ color: '#F31503' }}>
+                        Videos
+                    </span>
+                </Typography>
+                <Videos videos={[]} />
+            </Box>
+
+            <Box>
+                <Typography
+                    className="copyright"
+                    variant="body2"
+                    align="center"
+                    sx={{
+                        position: 'fixed',
+                        left: 0,
+                        bottom: 0,
+                        width: '100%',
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        color: '#fff',
+                        py: { sx: 1, md: 0 },
+                        zIndex: 1000
+                    }}
+                >
+                    Copyright@2023 CodeVivek. All rights reserved
+                </Typography>
+            </Box>
         </Stack >
     )
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
